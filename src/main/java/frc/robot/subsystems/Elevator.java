2d24f7583650fc15d42da2744ecd134da837643b
Gavin P <gavinjpeeler@gmail.com>
2023-01-10 18:42:41 -0800

    WIP elevator subsystem

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -5,18 +5,33 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Elevator extends SubsystemBase {
   /** Creates a new Elevator. */
   // Initialize 2 elevator motors as an array
+  public final TalonFX[] elevatorMotors = {
+    new TalonFX(Constants.Elevator.elevatorMotorLeft), new TalonFX(Constants.Elevator.elevatorMotorRight)
+  };
   // 0 = left motor, 1 = right motor
   // We will likely use TalonFX motors
+  private final double kF = 0;
+  private final double KP = 0.2;
   // Delcare constants for F and P for PIDF control
+  private boolean elevatorClimbState;
+
   // Boolean for elevator climb state, maybe double for holdPosition? (not sure what that is)
   // Int for desired height, 0 = no desired height/ground, 1 = middle nodes, 2 = top nodes
 
   public Elevator() {
     // Config both motors using a for loop on the array
+    for(TalonFX motor : elevatorMotors){
+      motor.configFactoryDefault();
+      motor.setSelectedSensorPosition(0);
+      motor.setNeutralMode(NeutralMode.brake)
+    }
+    elevatorMotors[0].setInverted(true);
+    elevatorMotors[1].setInverted(true);
     // Invert both motors
     // Follower statement for right motor to follow left motor speed
     // Config F and P for only the left motor for now cause thats how its done in Carbon for some reason, change it later if necessary
@@ -26,6 +41,7 @@ public class Elevator extends SubsystemBase {
   // Function for setting both motors into neutral mode
   // Function for setting the percent output of the motors
   // Getter methods for motor current, voltage, and output, input motor index
+  // Setter methods for motor voltage and output
 
   /** Getter methods for position, height, and velocity
    * Position and velocity will check for both motor's health
