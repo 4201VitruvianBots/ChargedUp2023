5413abcec70c88a9c149b9f85823f2eb0af3d7ab
Matthew Ochoa <matthewochoacoc@gmail.com>
2023-01-10 19:43:10 -0800

    Basic elevator subsystem code

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,6 +4,14 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -16,7 +24,7 @@ public class Elevator extends SubsystemBase {
   // 0 = left motor, 1 = right motor
   // We will likely use TalonFX motors
   private final double kF = 0;
-  private final double KP = 0.2;
+  private final double kP = 0.2;
   // Delcare constants for F and P for PIDF control
   private boolean elevatorClimbState;
 
@@ -28,18 +36,47 @@ public class Elevator extends SubsystemBase {
     for(TalonFX motor : elevatorMotors){
       motor.configFactoryDefault();
       motor.setSelectedSensorPosition(0);
-      motor.setNeutralMode(NeutralMode.brake)
+      motor.setNeutralMode(NeutralMode.Brake);
+      motor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+      motor.configSupplyCurrentLimit(
+        new SupplyCurrentLimitConfiguration(true, 35, 60, 0.1));
     }
+
     elevatorMotors[0].setInverted(true);
     elevatorMotors[1].setInverted(true);
     // Invert both motors
     // Follower statement for right motor to follow left motor speed
+    elevatorMotors[1].set(TalonFXControlMode.Follower, elevatorMotors[0].getDeviceID());
     // Config F and P for only the left motor for now cause thats how its done in Carbon for some reason, change it later if necessary
+    elevatorMotors[0].config_kF(0, kF);
+    elevatorMotors[0].config_kP(0, kP);
     // Set status frame period for both motors, frame values 1 and 2
+    elevatorMotors[0].setStatusFramePeriod(1, 100);
+    elevatorMotors[0].setStatusFramePeriod(2, 100);
+
+    elevatorMotors[1].setStatusFramePeriod(1, 255);
+    elevatorMotors[1].setStatusFramePeriod(2, 255);
   }
 
   // Function for setting both motors into neutral mode
+  public void setElevatorNeutralMode(NeutralMode mode) {
+    elevatorMotors[0].setNeutralMode(mode);
+    elevatorMotors[1].setNeutralMode(mode);
+  }
+
+  public void setElevatorPercentOutput(double output) {
+    elevatorMotors[0].set(ControlMode.PercentOutput, output);
+  }
   // Function for setting the percent output of the motors
+
+  public double getElevatorMotorVoltage() {
+    return elevatorMotors[0].getMotorOutputVoltage();
+  }
+
+  public double getElevatorPercentOutput() {
+    return elevatorMotors[0].getMotorOutputPercent();
+  }
+
   // Getter methods for motor current, voltage, and output, input motor index
   // Setter methods for motor voltage and output
 
@@ -47,8 +84,15 @@ public class Elevator extends SubsystemBase {
    * Position and velocity will check for both motor's health
   */
 
-  // Rotations to inches conversion function
+  // Encoder counts to inches conversion function
+  public double rotationsToInches(double rotations) {
+    return rotations*1;
+  }
 
+  public double inchesToRotations(double inches) {
+    return inches*1;
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
