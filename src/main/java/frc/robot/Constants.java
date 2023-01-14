2790393e4239da54c05e09f91747fadebf59bb6d
Ronan B <rdavidbouza@gmail.com>
2023-01-13 18:19:43 -0800

    Switching Computers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,14 @@
 
 package frc.robot;
 
+import java.util.Map;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import frc.robot.utils.ModuleMap;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -17,3 +25,67 @@ public final class Constants {
     public static final int kDriverControllerPort = 0;
   }
 }
+public static final class CAN { // TODO Not real number change tbt//
+  public static final int pigeon = 9;
+
+  public static final int frontLeftCanCoder = 10;
+  public static final int frontRightCanCoder = 11;
+  public static final int backLeftCanCoder = 12;
+  public static final int backRightCanCoder = 13;
+
+  public static final int frontLeftDriveMotor = 20;
+  public static final int frontLeftTurnMotor = 21;
+  public static final int frontRightDriveMotor = 22;
+  public static final int frontRightTurnMotor = 23;
+  public static final int backLeftDriveMotor = 24;
+  public static final int backLeftTurnMotor = 25;
+  public static final int backRightDriveMotor = 26;
+  public static final int backRightTurnMotor = 27;
+}
+
+public static final class SwerveDrive {
+  public static final double kTrackWidth = Units.inchesToMeters(30);
+  public static final double kWheelBase = Units.inchesToMeters(30);
+
+  public static final Map<ModulePosition, Translation2d> kModuleTranslations =
+      Map.of(
+          ModulePosition.FRONT_LEFT, new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+          ModulePosition.FRONT_RIGHT, new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+          ModulePosition.BACK_LEFT, new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+          ModulePosition.BACK_RIGHT, new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+  public static final double frontLeftCANCoderOffset = 94.219;
+  public static final double frontRightCANCoderOffset = 132.363;
+  public static final double backLeftCANCoderOffset = 284.590;
+  public static final double backRightCANCoderOffset = 179.648;
+
+  public static final SwerveDriveKinematics kSwerveKinematics =
+      new SwerveDriveKinematics(
+          ModuleMap.orderedValues(kModuleTranslations, new Translation2d[0]));
+
+  public static final double kMaxSpeedMetersPerSecond = Units.feetToMeters(18);
+  public static final double kMaxRotationRadiansPerSecond = Math.PI * 2.0;
+  public static final double kMaxRotationRadiansPerSecondSquared = Math.PI * 2.0;
+
+  public static final double kP_X = 0.2;
+  public static final double kD_X = 0;
+  public static final double kP_Y = 0.2;
+  public static final double kD_Y = 0;
+  public static final double kP_Theta = 8;
+  public static final double kD_Theta = 0;
+
+  public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+      new TrapezoidProfile.Constraints(
+          kMaxRotationRadiansPerSecond, kMaxRotationRadiansPerSecondSquared);
+
+  public enum ModulePosition {
+    FRONT_LEFT,
+    FRONT_RIGHT,
+    BACK_LEFT,
+    BACK_RIGHT
+  
+  }
+
+    }
+    
+   
---
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,222 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.SwerveDrive.*;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.Pigeon2;
+import com.ctre.phoenix.unmanaged.Unmanaged;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.CAN;
+import frc.robot.utils.ModuleMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SwerveDrive extends SubsystemBase {
+
+  private final HashMap<ModulePosition, SwerveModule> m_swerveModules =
+      new HashMap<>(
+          Map.of(
+              ModulePosition.FRONT_LEFT,
+                  new SwerveModule(
+                      ModulePosition.FRONT_LEFT,
+                      new TalonFX(CAN.frontLeftTurnMotor),
+                      new TalonFX(CAN.frontLeftDriveMotor),
+                      new CANCoder(CAN.frontLeftCanCoder),
+                      frontLeftCANCoderOffset),
+              ModulePosition.FRONT_RIGHT,
+                  new SwerveModule(
+                      ModulePosition.FRONT_RIGHT,
+                      new TalonFX(CAN.frontRightTurnMotor),
+                      new TalonFX(CAN.frontRightDriveMotor),
+                      new CANCoder(CAN.frontRightCanCoder),
+                      frontRightCANCoderOffset),
+              ModulePosition.BACK_LEFT,
+                  new SwerveModule(
+                      ModulePosition.BACK_LEFT,
+                      new TalonFX(CAN.backLeftTurnMotor),
+                      new TalonFX(CAN.backLeftDriveMotor),
+                      new CANCoder(CAN.backLeftCanCoder),
+                      backLeftCANCoderOffset),
+              ModulePosition.BACK_RIGHT,
+                  new SwerveModule(
+                      ModulePosition.BACK_RIGHT,
+                      new TalonFX(CAN.backRightTurnMotor),
+                      new TalonFX(CAN.backRightDriveMotor),
+                      new CANCoder(CAN.backRightCanCoder),
+                      backRightCANCoderOffset)));
+                      
+                      public static final SwerveDriveKinematics kSwerveKinematics =
+                      new SwerveDriveKinematics(
+                          ModuleMap.orderedValues(kModuleTranslations, new Translation2d[0]));
+
+  private final Pigeon2 m_pigeon = new Pigeon2(CAN.pigeon);
+
+  private final SwerveDriveOdometry m_odometry =
+      new SwerveDriveOdometry(
+          SwerveDrive.kSwerveKinematics,
+          getHeadingRotation2d(),
+          getModulePositions(),
+          new Pose2d());
+
+  private PIDController m_xController = new PIDController(kP_X, 0, kD_X);
+  private PIDController m_yController = new PIDController(kP_Y, 0, kD_Y);
+  private ProfiledPIDController m_turnController =
+      new ProfiledPIDController(kP_Theta, 0, kD_Theta, kThetaControllerConstraints);
+
+  private double m_simYaw;
+
+  public SwerveDrive() {
+    m_pigeon.setYaw(0);
+  }
+
+  public void drive(
+      double throttle,
+      double strafe,
+      double rotation,
+      boolean isFieldRelative,
+      boolean isOpenLoop) {
+    throttle *= kMaxSpeedMetersPerSecond;
+    strafe *= kMaxSpeedMetersPerSecond;
+    rotation *= kMaxRotationRadiansPerSecond;
+
+    ChassisSpeeds chassisSpeeds =
+        isFieldRelative
+            ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                throttle, strafe, rotation, getHeadingRotation2d())
+            : new ChassisSpeeds(throttle, strafe, rotation);
+
+    Map<ModulePosition, SwerveModuleState> moduleStates =
+        ModuleMap.of(kSwerveKinematics.toSwerveModuleStates(chassisSpeeds));
+
+    SwerveDriveKinematics.desaturateWheelSpeeds(
+        ModuleMap.orderedValues(moduleStates, new SwerveModuleState[0]), kMaxSpeedMetersPerSecond);
+
+    for (SwerveModule module : ModuleMap.orderedValuesList(m_swerveModules))
+      module.setDesiredState(moduleStates.get(module.getModulePosition()), isOpenLoop);
+  }
+
+  public void setSwerveModuleStates(SwerveModuleState[] states, boolean isOpenLoop) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, kMaxSpeedMetersPerSecond);
+
+    for (SwerveModule module : ModuleMap.orderedValuesList(m_swerveModules))
+      module.setDesiredState(states[module.getModulePosition().ordinal()], isOpenLoop);
+  }
+
+  public void setSwerveModuleStatesAuto(SwerveModuleState[] states) {
+    setSwerveModuleStates(states, false);
+  }
+
+  public void setOdometry(Pose2d pose) {
+    m_odometry.resetPosition(pose, pose.getRotation());
+    m_pigeon.setYaw(pose.getRotation().getDegrees());
+  }
+
+  public double getHeadingDegrees() {
+    return Math.IEEEremainder(m_pigeon.getYaw(), 360);
+  }
+
+  public Rotation2d getHeadingRotation2d() {
+    return Rotation2d.fromDegrees(getHeadingDegrees());
+  }
+
+  public Pose2d getPoseMeters() {
+    return m_odometry.getEstimatedPosition();
+  }
+
+  public SwerveModule getSwerveModule(ModulePosition modulePosition) {
+    return m_swerveModules.get(modulePosition);
+  }
+
+  public Map<ModulePosition, SwerveModuleState> getModuleStates() {
+    Map<ModulePosition, SwerveModuleState> map = new HashMap<>();
+    for (ModulePosition i : m_swerveModules.keySet()) {
+      map.put(i, m_swerveModules.get(i).getState());
+    }
+    return map;
+  }
+  public SwerveModulePosition[] getModulePositions() {
+    return new SwerveModulePosition[] {
+        m_swerveModules.get(ModulePosition.FRONT_LEFT).getPosition(),
+        m_swerveModules.get(ModulePosition.FRONT_RIGHT).getPosition(),
+        m_swerveModules.get(ModulePosition.BACK_LEFT).getPosition(),
+        m_swerveModules.get(ModulePosition.BACK_RIGHT).getPosition()
+    };3
+  public PIDController getXPidController() {
+    return m_xController;
+  }
+
+  public PIDController getYPidController() {
+    return m_yController;
+  }
+
+  public ProfiledPIDController getThetaPidController() {
+    return m_turnController;
+  }
+
+  public void setNeutralMode(NeutralMode mode) {
+    for (SwerveModule module : m_swerveModules.values()) {
+      module.setDriveNeutralMode(mode);
+      module.setTurnNeutralMode(mode);
+    }
+  }
+
+  public SwerveDrivePoseEstimator getOdometry() {
+    return m_odometry;
+  }
+
+  public void updateOdometry() {
+    m_odometry.update(
+        getHeadingRotation2d(),
+        ModuleMap.orderedValues(getModuleStates(), new SwerveModuleState[0]));
+
+    for (SwerveModule module : ModuleMap.orderedValuesList(m_swerveModules)) {
+      Translation2d modulePositionFromChassis =
+          kModuleTranslations
+              .get(module.getModulePosition())
+              .rotateBy(getHeadingRotation2d())
+              .plus(getPoseMeters().getTranslation());
+      module.setModulePose(
+          new Pose2d(
+              modulePositionFromChassis,
+              module.getHeadingRotation2d().plus(getHeadingRotation2d())));
+    }
+  }
+
+  private void updateSmartDashboard() {}
+
+  @Override
+  public void periodic() {
+    updateOdometry();
+    updateSmartDashboard();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    ChassisSpeeds chassisSpeed =
+        kSwerveKinematics.toChassisSpeeds(
+            ModuleMap.orderedValues(getModuleStates(), new SwerveModuleState[0]));
+
+    m_simYaw += chassisSpeed.omegaRadiansPerSecond * 0.02;
+
+    Unmanaged.feedEnable(20);
+    m_pigeon.getSimCollection().setRawHeading(-Units.radiansToDegrees(m_simYaw));
+  }
+}
