a899f53c740e77d02b2fc6eb0735fbc5b2083c52
Miles Walker <mightymiles08@outlook.com>
2023-01-28 11:34:13 -0800

    PID for Intake is finished

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,43 +5,65 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
   private static final String Controlmode = null;
-  /** Creates a new Intake. */
-  private boolean isIntaking = false;
-
+/** Creates a new Intake. */
+  private boolean isIntaking = false; 
   private TalonFX intakeMotor = new TalonFX(Constants.Intake.intakeMotor);
-
+  private final PIDController intakePID = new PIDController(0.1, 0.01, 0.05);
   public Intake() {
-    // one motor for the intake
+      // one or two motors
 
+    //factory default cofigs
     intakeMotor.configFactoryDefault();
-    // factory default cofigs
-  }
+     intakePID.setTolerance(1);
+     intakePID.enableContinuousInput(-180, 180);
+     intakeMotor.setInverted(false);
 
-  public boolean getIntakeState() {
-    return isIntaking;
+    intakeMotor.setStatusFramePeriod(1, 255);
+    intakeMotor.setStatusFramePeriod(2, 255);
+    intakeMotor.setNeutralMode(NeutralMode.Brake);
+    intakeMotor.configVoltageCompSaturation(10);
+    intakeMotor.enableVoltageCompensation(true);
+  
+    
   }
-  // control mode function
-  public void setIntakeState(boolean state) {
-    isIntaking = state;
+  public void setIntakeSate(boolean state){
+    boolean isIntaking = state;
   }
+  public void setSetpoint(double setpoint) {
+    intakePID.setSetpoint(setpoint);
+}
+public double getMeasurement() {
+  return intakeMotor.getSelectedSensorPosition();
+}
+
+// control mode function 
+public boolean getIntakeState(boolean state){
+return isIntaking;
+}public boolean getIntakeState;
+
   // set percent output function
-  public void setIntakePercentOutput(double value) {
+  public void setIntakePercentOutput(double value){
     intakeMotor.set(ControlMode.PercentOutput, value);
   }
   // shuffleboard or smartdashboard funciton
   public void updateSmartDashboard() {
-    SmartDashboard.putBoolean("Intake", getIntakeState());
+    SmartDashboard.putBoolean("Intake", getIntakeState(false));
+    SmartDashboard.putNumber("getIntake", 1);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    setIntakePercentOutput(intakePID.calculate(getMeasurement()));
   }
 }
