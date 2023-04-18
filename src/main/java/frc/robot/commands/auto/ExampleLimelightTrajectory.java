dbbde33e3f2b9dc2b0487a67c6af97af9c894567
Unknown <jonathandao0@gmail.com>
2023-04-17 22:36:56 -0700

    Code Cleanup

--- src/main/java/frc/robot/commands/auto/ExampleLimelightTrajectory.java
+++ src/main/java/frc/robot/commands/auto/ExampleLimelightTrajectory.java
@@ -5,6 +5,10 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.Constants;
+import frc.robot.Constants.INTAKE.INTAKE_STATE;
+import frc.robot.Constants.STATE_HANDLER;
+import frc.robot.Constants.VISION.CAMERA_SERVER;
+import frc.robot.Constants.VISION.PIPELINE;
 import frc.robot.commands.DelayedInterruptingCommand;
 import frc.robot.commands.intake.AutoSetIntakeSetpoint;
 import frc.robot.commands.statehandler.AutoSetSetpoint;
@@ -41,10 +45,7 @@ public class ExampleLimelightTrajectory extends SequentialCommandGroup {
         new SetSwerveOdometry(
             swerveDrive, m_trajectories.get(0).getInitialHolonomicPose(), fieldSim),
         new PlotAutoTrajectory(fieldSim, pathName, m_trajectories),
-        new InstantCommand(
-            () ->
-                vision.setPipeline(
-                    Constants.VISION.CAMERA_SERVER.INTAKE, Constants.VISION.PIPELINE.CUBE.get())),
+        new InstantCommand(() -> vision.setPipeline(CAMERA_SERVER.INTAKE, PIPELINE.CUBE.get())),
         new ParallelDeadlineGroup(
             // Use the trajectory's normal time as the deadline
             new WaitCommand(m_trajectories.get(0).getTotalTimeSeconds()),
@@ -52,24 +53,16 @@ public class ExampleLimelightTrajectory extends SequentialCommandGroup {
             new DelayedInterruptingCommand(
                 swerveCommands.get(0),
                 new DriveForwardWithVisionInput(swerveDrive, vision, () -> 0.4)
-                    .until(
-                        () ->
-                            intake.getIntakeState() == Constants.INTAKE.INTAKE_STATE.HOLDING_CUBE),
+                    .until(() -> intake.getIntakeState() == INTAKE_STATE.HOLDING_CUBE),
                 3, // Put the delay for the limelight here
-                () -> vision.getValidTarget(Constants.VISION.CAMERA_SERVER.INTAKE)),
+                () -> vision.getValidTarget(CAMERA_SERVER.INTAKE)),
             // Move the wrist/intake at the same time.
             new SequentialCommandGroup(
                 new WaitCommand(0.75),
                 new ParallelCommandGroup(
                     new AutoSetSetpoint(
-                        stateHandler,
-                        elevator,
-                        wrist,
-                        Constants.STATE_HANDLER.SETPOINT.INTAKING_LOW_CUBE),
+                        stateHandler, elevator, wrist, STATE_HANDLER.SETPOINT.INTAKING_LOW_CUBE),
                     new AutoSetIntakeSetpoint(
-                        intake,
-                        Constants.INTAKE.INTAKE_STATE.INTAKING_CUBE,
-                        vision,
-                        swerveDrive)))));
+                        intake, INTAKE_STATE.INTAKING_CUBE, vision, swerveDrive)))));
   }
 }
