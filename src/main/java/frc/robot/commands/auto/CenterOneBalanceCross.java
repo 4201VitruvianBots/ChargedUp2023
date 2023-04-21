e06390272cb2f463376dc3a1a333e84cbfed11d8
Jonathan Dao <jonathandao0@gmail.com>
2023-04-21 11:09:21 -0700

    pre-qual 104

--- src/main/java/frc/robot/commands/auto/CenterOneBalanceCross.java
+++ src/main/java/frc/robot/commands/auto/CenterOneBalanceCross.java
@@ -38,8 +38,8 @@ public class CenterOneBalanceCross extends SequentialCommandGroup {
       Vision vision,
       StateHandler stateHandler) {
 
-    double maxVel = Units.feetToMeters(8);
-    double maxAccel = Units.feetToMeters(8);
+    double maxVel = Units.feetToMeters(6);
+    double maxAccel = Units.feetToMeters(6);
     if (RobotBase.isSimulation()) {
       maxVel = Units.feetToMeters(4);
       maxAccel = Units.feetToMeters(4);
